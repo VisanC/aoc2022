1eefb8a01bda8a1e022df39c0b9f08e5e96d0aa8
VisanC <tudorcosmin.visan@gmail.com>
2022-12-08 13:09:32 +0200

    day8 - rename for part 2

--- day8/day8/src/Main.java
+++ day8/day8/src/Main.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 
 public class Main {
 
-    public static int doesItBlockRowLeft(List<List<Integer>> mat, int row, int column, int target) {
+    public static int rangerRowLeft(List<List<Integer>> mat, int row, int column, int target) {
 
         if (mat.get(row).get(column) >= target) {
             return 1;
@@ -13,10 +13,10 @@ public class Main {
         if (row == 0 || row == mat.size() - 1) {
             return 1;
         }
-        return 1+doesItBlockRowLeft(mat, row - 1, column, target);
+        return 1 + rangerRowLeft(mat, row - 1, column, target);
     }
 
-    public static int doesItBlockRowRight(List<List<Integer>> mat, int row, int column, int target) {
+    public static int rangeRowRight(List<List<Integer>> mat, int row, int column, int target) {
 
         if (mat.get(row).get(column) >= target) {
             return 1;
@@ -24,37 +24,38 @@ public class Main {
         if (row == 0 || row == mat.size() - 1) {
             return 1;
         }
-        return  1+doesItBlockRowRight(mat, row + 1, column, target);
+        return 1 + rangeRowRight(mat, row + 1, column, target);
     }
 
-    public static int doesItBlockColRight(List<List<Integer>> mat, int row, int column, int target) {
+    public static int rangeColRight(List<List<Integer>> mat, int row, int column, int target) {
         if (mat.get(row).get(column) >= target) {
             return 1;
         }
         if (column == 0 || column == mat.get(0).size() - 1) {
             return 1;
         }
-        return 1+doesItBlockColRight(mat, row, column + 1, target);
+        return 1 + rangeColRight(mat, row, column + 1, target);
     }
-    public static int doesItBlockColLeft(List<List<Integer>> mat, int row, int column, int target) {
-        if ( mat.get(row).get(column) >= target) {
+
+    public static int rangeColLeft(List<List<Integer>> mat, int row, int column, int target) {
+        if (mat.get(row).get(column) >= target) {
             return 1;
         }
         if (column == 0 || column == mat.get(0).size() - 1) {
             return 1;
         }
-        return 1+doesItBlockColLeft(mat, row, column - 1, target);
+        return 1 + rangeColLeft(mat, row, column - 1, target);
     }
 
-    public static int isVisible(List<List<Integer>> mat, int row, int column) {
+    public static int range(List<List<Integer>> mat, int row, int column) {
         boolean isVisible = false;
         if (row == 0 || column == 0 || row == mat.size() - 1 || column == mat.get(0).size() - 1) {
             return 1;
         }
         int ret = 0;
         int target = mat.get(row).get(column);
-        ret= doesItBlockRowLeft(mat,row-1,column,target) * doesItBlockColLeft(mat,row,column-1,target)*
-                doesItBlockRowRight(mat,row+1,column,target) * doesItBlockColRight(mat,row,column+1,target);
+        ret = rangerRowLeft(mat, row - 1, column, target) * rangeColLeft(mat, row, column - 1, target) *
+                rangeRowRight(mat, row + 1, column, target) * rangeColRight(mat, row, column + 1, target);
         return ret;
     }
 
@@ -69,12 +70,10 @@ public class Main {
         System.out.println(visibleTrees);
         for (int i = 0; i < mat.size(); i++) {
             for (int j = 0; j < mat.get(0).size(); j++) {
-                int a = isVisible(mat, i, j);
-                if(a>visibleTrees){
-                    visibleTrees=a;
+                int a = range(mat, i, j);
+                if (a > visibleTrees) {
+                    visibleTrees = a;
                 }
-                if (a == 0)
-                    System.out.println(a + "  coords " + i + " " + j + " value " + mat.get(i).get(j));
             }
         }
         System.out.println(visibleTrees);
